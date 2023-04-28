bfa6be9a6a8571bc0dc7ee9c800b8b2d45e6b675
dalilasampaio <dalilasampaio6@gmail.com>
2023-04-27 23:14:46 -0300

    Testing database connection.

---
+++ backend/src/test/java/br/com/dalila/workmanager/WorkmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dalila.workmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
